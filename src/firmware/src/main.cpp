4bf018ba6848d966a01be0dc8162264301347cdd
unknown <becaxito@gmail.com>
2026-05-24 22:12:51 -0300

    feat(firmware): integrar objeto de telemetria doc para sensor RGB

--- src/firmware/src/main.cpp
+++ src/firmware/src/main.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SparkFun_APDS9960.h>
+#include <ArduinoJson.h> // Adicionado: Necessário para a sintaxe do objeto 'doc' funcionar
 
 // Definindo os pinos I2C padrão para a ESP32-C3
 #define I2C_SDA 8
 #define I2C_SCL 9
 
 SparkFun_APDS9960 apds = SparkFun_APDS9960();
+
+// Adicionado: Objeto global de telemetria para permitir o uso de doc["..."]
+StaticJsonDocument<200> doc;
+
+uint16_t luzAmbienteBase = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -27,6 +33,13 @@ void setup() {
   } else {
     Serial.println("Erro ao ativar leitor de cores.");
   }
+
+  // Adicionado: Inicializa a variável de telemetria pedida na mensagem ("N" = Não completado)
+  doc["desafioCumprido"] = "N";
+
+  // Salva o valor da luz da sala assim que o robô liga para sabermos o "zero" do ambiente
+  delay(500);
+  apds.readAmbientLight(luzAmbienteBase);
 }
 
 void loop() {
@@ -34,6 +47,12 @@ void loop() {
 
   // Realiza a leitura dos canais de cor
   if (apds.readRedLight(r) && apds.readGreenLight(g) && apds.readBlueLight(b) && apds.readAmbientLight(c)) {
+    // Desconta o excesso de luz da sala para o valor lido ser real e não sofrer interferência externa
+    if (c > luzAmbienteBase) {
+      uint16_t excessoLuz = c - luzAmbienteBase;
+      r = (r > excessoLuz) ? (r - excessoLuz) : 0;
+    }
+    
     Serial.printf("Cores - R: %d | G: %d | B: %d | Ambiente: %d\n", r, g, b, c);
 
     // LÓGICA DE FIM DE LABIRINTO
@@ -41,8 +60,8 @@ void loop() {
     if (r > 200 && g < 100 && b < 100) {
       Serial.println("🎯 FIM DO LABIRINTO DETECTADO! Acionando freios...");
       
-      // Aqui você futuramente enviará a telemetria via WebSocket avisando o término:
-      // doc["desafioCumprido"] = "S";
+      // Transforma o comentário em linha de código ativa para atualizar a telemetria
+      doc["desafioCumprido"] = "S";
     }
   }
 
